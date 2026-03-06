86d2ea9f603801ced61155e8cdb44aedfb82bff8
Dindb-dong <dongwook443@yonsei.ac.kr>
2026-03-06 16:26:04 +0900

    feat: hide top navigation links during battle

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './TopBar.css';
 
 const TopBar = () => {
+  const location = useLocation();
+  const isBattleRoute = location.pathname === "/battle";
+
   return (
-    <div className="top-bar">
+    <div className={`top-bar ${isBattleRoute ? "top-bar--battle" : ""}`}>
       <div className="logo">
         <Link to="/">Yakemon!</Link>
       </div>
-      <div className="nav-links">
-        <Link to="/leaderboard">리더보드</Link>
-        <Link to="/mypage">마이페이지</Link>
-      </div>
+      {!isBattleRoute && (
+        <div className="nav-links">
+          <Link to="/leaderboard">리더보드</Link>
+          <Link to="/mypage">마이페이지</Link>
+        </div>
+      )}
     </div>
   );
 };
